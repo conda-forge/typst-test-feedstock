ffbb5eeea6d6513695dace0812df9bb974788df2
Adrian Freund <adrian@freund.io>
2024-06-26 14:12:38 +0200

    Roll back to ci-semi-stable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ cargo-bundle-licenses ^
     --format yaml ^
     --output THIRDPARTY_LICENSES.yaml || goto :error
 
-cargo install --no-track --locked --root "%PREFIX%" --path crates\typst-test-cli || goto :error
+cargo install --no-track --locked --root "%PREFIX%" --path crates\typst-test || goto :error
 
 goto :EOF
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY_LICENSES.yaml
 
-cargo install --no-track --locked --root "$PREFIX" --path crates/typst-test-cli
+cargo install --no-track --locked --root "$PREFIX" --path crates/typst-test
 
 "$STRIP" "$PREFIX/bin/typst-test"
